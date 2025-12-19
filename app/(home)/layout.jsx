aa99995ee81a2f41c8d028cbd8456de8e476386f
mohd-hassan17 <mohdhassana04@gmail.com>
2025-12-19 23:17:29 +0530

    cmn issue

--- app/(home)/layout.jsx
+++ app/(home)/layout.jsx
@@ -4,14 +4,12 @@ import React from "react";
 
 export const dynamic = "force-dynamic";
 
-
 const RootLayout = async({ children }) => {
 
-  
   const userRole = await currentUserRole();
 
   return (
-    <main className="flex flex-col min-h-screen max-h-screen">
+    <main className="flex flex-col h-full">
       <Navbar userRole={userRole} />
       <div className="flex-1 flex flex-col px-4 pb-4">
            <div className="absolute inset-0 -z-10 h-full w-full bg-background dark:bg-[radial-gradient(#393e4a_1px,transparent_1px)] dark:bg-size-[16px_16px] bg-[radial-gradient(#dadde2_1px,transparent_1px)] bg-size-[16px_16px]" />
