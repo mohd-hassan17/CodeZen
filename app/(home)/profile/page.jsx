2877293960b297789e67c88e529c217cf764ad1c
mohd-hassan17 <mohdhassana04@gmail.com>
2025-12-19 23:26:14 +0530

    profile page issue solved

--- app/(home)/profile/page.jsx
+++ app/(home)/profile/page.jsx
@@ -5,10 +5,25 @@ import ProfileStats from "@/modules/profile/components/profile-stats";
 import SolvedProblems from "@/modules/profile/components/solved-problems";
 import SubmissionsHistory from "@/modules/profile/components/submission-history";
 import UserInfoCard from "@/modules/profile/components/user-info-card";
+import { currentUser } from '@clerk/nextjs/server'
 
 const ProfilePage = async () => {
 
     const profileData = await getCurrentUserData();
+    const user = await currentUser();
+
+    if(!user){
+      return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-32">
+          <h2 className="text-2xl font-semibold mb-4">User not found</h2>
+          <p className="text-zinc-600 dark:text-zinc-400">
+            The profile you are looking for does not exist.
+          </p>
+        </div>
+      )
+    }
+
+
 
     // console.log(profileData);
 
